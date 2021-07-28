194172e17719da044a75b50a632069c9758a81c7
Felipe Gómez <felipe.gomezc@outlook.com>
2021-07-28 18:33:40 -0500

    Cambio de color en el fondo del terminal

--- Program.cs
+++ Program.cs
@@ -7,8 +7,11 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string lecturaLineaConsola="";
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine("Hello World!");
             lecturaLineaConsola = Console.ReadLine();
+            Console.WriteLine("El texto escrito fue: "+lecturaLineaConsola);
         }
     }
 }
